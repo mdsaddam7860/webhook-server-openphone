bb9e4d5f3f33831cca3b789715de35261826a1d4
Md Saddam <mdsonu7860@outlook.com>
2025-11-07 20:18:35 +0530

    Add dotenv configuration and update root route response

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from "express";
+// config.js
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from root folder explicitly
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 import { handleWebhook } from "./index.js";
 
@@ -10,7 +18,7 @@ const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
-app.get("/", (req, res) => res.send("<h1>🚀 App is running</h1>"));
+app.get("/", (req, res) => res.send("<h1>App is running</h1>"));
 app.post("/webhook", handleWebhook);
 
 export { app, router };
