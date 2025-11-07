14d06103c2c956ddca0b1145f235446fedc6d549
Md Saddam <mdsonu7860@outlook.com>
2025-11-07 20:25:12 +0530

    Refactor configuration loading and update app response message

--- src/app.js
+++ src/app.js
@@ -1,15 +1,5 @@
+import "./configs/config.js";
 import express from "express";
-// config.js
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Load .env from root folder explicitly
-dotenv.config({ path: path.join(__dirname, ".env") });
-
 import { handleWebhook } from "./index.js";
 
 const router = express.Router();
@@ -18,7 +8,7 @@ const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
-app.get("/", (req, res) => res.send("<h1>App is running</h1>"));
+app.get("/", (req, res) => res.send("<h1>🚀 App is running</h1>"));
 app.post("/webhook", handleWebhook);
 
 export { app, router };
---
+++ src/configs/config.js
@@ -0,0 +1,10 @@
+// config.js
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from root folder explicitly
+dotenv.config({ path: path.join(__dirname, "../../.env") });
