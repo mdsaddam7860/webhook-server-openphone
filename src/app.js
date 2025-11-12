3103eec48be6c472cb38ba5c0843d9185e82c42e
Md Saddam <mdsonu7860@outlook.com>
2025-11-12 23:15:10 +0530

    Comment out webhook route in app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,6 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.get("/", (req, res) => res.send("<h1>App is running</h1>"));
-app.post("/webhook", handleWebhook2);
+// app.post("/webhook", handleWebhook2);
 
 export { app, router };
